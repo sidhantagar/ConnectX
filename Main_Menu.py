02f7fa01bf24361abafcf73f444b593228cec214
Sidhant Agarwal <sidhant11136@gmail.com>
2020-08-24 10:43:18 +0530

    Update Main_Menu.py

--- Main_Menu.py
+++ Main_Menu.py
@@ -1,7 +1,12 @@
 import sys
 import time
 import pygame
+from new_game import*
 from drawbuttons import *
+
+#Initial values
+player_number = 1
+mode = 2
 
 def main_menu(font):
     draw_bordered_rounded_rect (screen, (100,200,200,70),(0,0,255),(255,255,0),10,5)
@@ -20,9 +25,12 @@ def main_menu(font):
                 (mouse_pos_x, mouse_pos_y) = event.pos
                 #Checking for button1
                 if mouse_pos_x > 99 and mouse_pos_x < 301 and mouse_pos_y > 199 and mouse_pos_y < 271:
-                    print("Button 1 clicked!")
+                    ret = newGame(player_number,mode)
+                    if ret== 0: 
+                        return
                 elif mouse_pos_x > 99 and mouse_pos_x <301 and mouse_pos_y > 299 and mouse_pos_y <371:
                     print("Button 2 clicked!")
+
 
 pygame.init()
 pygame.font.init()
